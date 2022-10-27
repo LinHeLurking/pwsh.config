4d5834a6136dcdc2a5ea1cc01e9c6e39760f14a7
LinHeLurking <LinHe.Lurking@gmail.com>
2022-10-27 17:09:28 +0800

    Color change;

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,16 @@
+Set-Alias -Name vim -Value nvim
+
+$ENV:HTTP_PROXY = "http://127.0.0.1:1089"
+$ENV:HTTPS_PROXY = "http://127.0.0.1:1089"
+
 Import-Module posh-git
 $env:POSH_GIT_ENABLED = $true
+Start-Job -ScriptBlock { gpgconf.exe --launch gpg-agent } | Out-Null
+
 Import-Module DockerCompletion
-Start-Job -ScriptBlock {gpgconf.exe --launch gpg-agent} | Out-Null
+
+$vs_install_path = "E:\Program Files\Microsoft Visual Studio\2022\Community"
+Import-Module "$vs_install_path\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Enter-VsDevShell -VsInstallPath $vs_install_path -SkipAutomaticLocation -Arch amd64 -HostArch amd64
+
 oh-my-posh init pwsh --config "C:/Users/$env:USERNAME/Documents/PowerShell/powerlevel10k_rainbow_refined.omp.json" | Invoke-Expression
