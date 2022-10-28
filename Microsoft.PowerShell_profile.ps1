93e6605c7f6c9b7ddecc46adc76c6f10c34ee54e
LinHeLurking <LinHe.Lurking@gmail.com>
2022-10-28 10:15:09 +0800

    Wrapp git, docker module and invoke on demand;

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,11 +3,7 @@ Set-Alias -Name vim -Value nvim
 $ENV:HTTP_PROXY = "http://127.0.0.1:1089"
 $ENV:HTTPS_PROXY = "http://127.0.0.1:1089"
 
-Import-Module posh-git
-$env:POSH_GIT_ENABLED = $true
 Start-Job -ScriptBlock { gpgconf.exe --launch gpg-agent } | Out-Null
-
-Import-Module DockerCompletion
 
 function Use-VS
 {
@@ -15,5 +11,18 @@ function Use-VS
     Import-Module "$vs_install_path\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
     Enter-VsDevShell -VsInstallPath $vs_install_path -SkipAutomaticLocation -Arch amd64 -HostArch amd64
 }
+
+function Use-Git
+{
+    Import-Module posh-git
+    $env:POSH_GIT_ENABLED = $true
+}
+
+function Use-Docker
+{
+    Import-Module DockerCompletion
+}
+
+Use-Git
 
 oh-my-posh init pwsh --config "C:/Users/$env:USERNAME/Documents/PowerShell/powerlevel10k_rainbow_refined.omp.json" | Invoke-Expression
