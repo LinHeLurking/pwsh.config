a399afa52cbb4b4071e26c283964b3823871a249
LinHeLurking <LinHe.Lurking@gmail.com>
2022-11-08 18:26:08 +0800

    fix: remove hard coded vs installation directory;

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,22 +4,19 @@ $ENV:HTTP_PROXY = "http://127.0.0.1:1089"
 $ENV:HTTPS_PROXY = "http://127.0.0.1:1089"
 
 Start-Job -ScriptBlock { gpgconf.exe --launch gpg-agent } | Out-Null
-
-function Use-VS
-{
-    $vs_install_path = "E:\Program Files\Microsoft Visual Studio\2022\Community"
-    Import-Module "$vs_install_path\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-    Enter-VsDevShell -VsInstallPath $vs_install_path -SkipAutomaticLocation -Arch amd64 -HostArch amd64
+function Use-VS {
+    $VsWhere = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    $VsInstallDir = & $VsWhere -latest -property installationPath
+    Import-Module "$VsInstallDir\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+    Enter-VsDevShell -VsInstallPath $VsInstallDir -SkipAutomaticLocation -Arch amd64 -HostArch amd64
 }
 
-function Use-Git
-{
+function Use-Git {
     Import-Module posh-git
     $env:POSH_GIT_ENABLED = $true
 }
 
-function Use-Docker
-{
+function Use-Docker {
     Import-Module DockerCompletion
 }
 
