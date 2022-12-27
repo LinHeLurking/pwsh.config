a2fb29367cdca990ee26a4c5fa4f4e0ce4be416b
LinHeLurking <LinHe.Lurking@gmail.com>
2022-12-27 20:31:41 +0800

    fix: no specify arch;

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,7 @@ function Use-VS {
     $VsWhere = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
     $VsInstallDir = & $VsWhere -latest -property installationPath
     Import-Module "$VsInstallDir\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-    Enter-VsDevShell -VsInstallPath $VsInstallDir -SkipAutomaticLocation -Arch amd64 -HostArch amd64
+    Enter-VsDevShell -VsInstallPath $VsInstallDir -SkipAutomaticLocation
 }
 
 function Use-Git {
